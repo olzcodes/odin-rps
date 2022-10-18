ec04b369ba524aa6c57a43a3885ce10bd7874681
Oliver L <oliver.lkm@gmail.com>
2022-10-18 13:40:50 +1100

    Add visual feedback and post-game options

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const computerScore = document.querySelector("#score-computer");
 const tieScore = document.querySelector("#score-tie");
 const playerScore = document.querySelector("#score-player");
-const result = document.querySelector(".result");
-const UIbuttons = document.querySelectorAll(".player-btn");
+const roundDisplay = document.querySelector(".round");
+const computerButtons = document.querySelectorAll(".computer-btn");
+const message = document.querySelector(".message");
+const playerButtons = document.querySelectorAll(".player-btn");
 
 const weapons = ["rock", "paper", "scissors"];
 
@@ -17,21 +19,42 @@ const getComputerSelection = function () {
   return weapons[number];
 };
 
+const showComputerSelection = function (selection) {
+  computerButtons.forEach((button) => {
+    if (button.classList.value.split(" ")[1] === selection) {
+      button.style.background = "plum";
+    }
+    setTimeout(() => {
+      button.style.background = "";
+    }, 2000);
+  });
+};
+
+const showPlayerSelection = function (button) {
+  button.style.background = "plum";
+  setTimeout(() => {
+    button.style.background = "";
+  }, 2000);
+};
+
 let playerSelection;
 let computerSelection;
 let round = 0;
 let score = { computer: 0, tie: 0, player: 0 };
 
-UIbuttons.forEach((button) =>
+playerButtons.forEach((button) =>
   button.addEventListener("click", function (e) {
-    // console.log(e.target);
     playerSelection = this.classList.value.split(" ")[1];
     computerSelection = getComputerSelection();
 
     // Round counter
-    console.log(`Round ${(round += 1)}`);
+    round += 1;
+    console.log(`Round ${round}`);
+    roundDisplay.textContent = `round ${round}`;
 
     // Play one round and update score
+    showComputerSelection(computerSelection);
+    showPlayerSelection(this);
     const roundWinner = playRound(playerSelection, computerSelection);
     score[roundWinner]++;
     computerScore.textContent = score["computer"];
@@ -41,8 +64,7 @@ UIbuttons.forEach((button) =>
     console.log(`------------------------------------`);
 
     if (round === 5) {
-      game();
-      round = 0;
+      checkWinner();
     }
   })
 );
@@ -55,33 +77,56 @@ const playRound = function (playerSelection, computerSelection) {
   // Check for tie
   if (playerSelection === computerSelection) {
     console.log(`TIE`);
-    result.textContent = `TIE`;
+    message.textContent = `TIE`;
     return "tie";
 
     // Check for player win
   } else if (playerSelection === winningPlays[computerSelection]) {
     console.log(`PLAYER wins!`);
-    result.textContent = `PLAYER wins!`;
+    message.textContent = `PLAYER wins!`;
     return "player";
 
     // Computer wins
   } else {
     console.log(`COMPUTER wins!`);
-    result.textContent = `COMPUTER wins!`;
+    message.textContent = `COMPUTER wins!`;
     return "computer";
   }
 };
 
-const game = function () {
+const checkWinner = function () {
   // Check if there was a final winner
   if (score["player"] > score["computer"]) {
-    console.log(`<<<<< PLAYER wins the game! >>>>>`);
-    result.textContent = `<<<<< PLAYER wins the game! >>>>>`;
+    console.log(`< < < PLAYER wins the game! > > >`);
+    message.textContent = `< < < PLAYER wins the game! > > >`;
   } else if (score["player"] < score["computer"]) {
-    console.log(`<<<<< COMPUTER wins the game! >>>>>`);
-    result.textContent = `<<<<< COMPUTER wins the game! >>>>>`;
+    console.log(`< < < COMPUTER wins the game! > > >`);
+    message.textContent = `< < < COMPUTER wins the game! > > >`;
   } else {
-    console.log(`<<<<< TIE >>>>>`);
-    result.textContent = `<<<<< TIE >>>>>`;
+    console.log(`< < < TIE > > >`);
+    message.textContent = `< < < TIE > > >`;
   }
+
+  setTimeout(afterGame, 1000);
+};
+
+const afterGame = function () {
+  let playAgain = confirm(`play again?`);
+  if (playAgain) {
+    resetGame();
+  } else {
+    console.log(`Thank you for playing!`);
+    message.textContent = `thank you for playing :)`;
+  }
+};
+
+const resetGame = function () {
+  console.clear();
+  round = 0;
+  score = { computer: 0, tie: 0, player: 0 };
+  roundDisplay.textContent = "start";
+  computerScore.textContent = 0;
+  tieScore.textContent = 0;
+  playerScore.textContent = 0;
+  message.textContent = "choose your weapon";
 };
